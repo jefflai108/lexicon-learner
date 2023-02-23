3355a0040f6b9239564b4ef8ba11f3905ca47030
jefflai108 <jefflai108@gmail.com>
2023-02-22 21:25:46 -0500

    update

--- data_utils/filter_manifest.py
+++ data_utils/filter_manifest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def read_train_tsv(target_tsv, source_tsv, output_target_tsv, output_source_tsv, tgt_frame_filter_threshold=100): 
+def filter_train_mined_tsv(target_tsv, source_tsv, output_target_tsv, output_source_tsv, tgt_frame_filter_threshold=100): 
 
     src_df = pd.read_table(source_tsv, sep='\t', header=0)
     tgt_df = pd.read_table(target_tsv, sep='\t', header=0)
@@ -24,7 +24,7 @@ def read_train_tsv(target_tsv, source_tsv, output_target_tsv, output_source_tsv,
     out_src_f.close()
 
 
-def read_valid_tsv(target_tsv, source_tsv, output_target_tsv, output_source_tsv, tgt_frame_filter_threshold=100): 
+def filter_valid_vp_tsv(target_tsv, source_tsv, output_target_tsv, output_source_tsv, tgt_frame_filter_threshold=100): 
 
     src_df = pd.read_table(source_tsv, sep='\t', header=0)
     tgt_df = pd.read_table(target_tsv, sep='\t', header=0)
@@ -47,6 +47,44 @@ def read_valid_tsv(target_tsv, source_tsv, output_target_tsv, output_source_tsv,
     out_tgt_f.close()
     out_src_f.close()
 
+
+def filter_test_epst_tsv(target_tsv, source_tsv, transcription, output_target_tsv, output_source_tsv, output_transcription, tgt_frame_filter_threshold=100): 
+
+    src_df = pd.read_table(source_tsv, sep='\t', header=0)
+    tgt_df = pd.read_table(target_tsv, sep='\t', header=0)
+    with open(transcription, 'r') as f: 
+        tran_content = f.readlines()
+    tran_content = [x.strip('\n') for x in tran_content]
+
+    out_tgt_f  = open(output_target_tsv, 'w')
+    out_src_f  = open(output_source_tsv, 'w')
+    out_tran_f = open(output_transcription, 'w') 
+
+    out_tgt_f.write('id\tsrc_audio\tsrc_n_frames\ttgt_audio\ttgt_n_frames\n')
+    out_src_f.write('id\ttgt_text\n')
+
+    num_examples = len(src_df['tgt_text'])
+
+    total_frame = 0
+    total_cnt = 0
+    for i in range(num_examples): 
+        tgt_n_frames = tgt_df['src_n_frames'][i]
+
+        if tgt_n_frames <= tgt_frame_filter_threshold: 
+            out_tgt_f.write('%s\t%s\t%s\t%s\t%s\n' % (tgt_df['id'][i], tgt_df['src_audio'][i], tgt_df['src_n_frames'][i], tgt_df['tgt_audio'][i], tgt_df['tgt_n_frames'][i]))
+            out_src_f.write('%s\t%s\n' % (src_df['id'][i], src_df['tgt_text'][i]))
+            out_tran_f.write('%s\n' % tran_content[i])
+
+            total_cnt += 1 
+            total_frame += tgt_n_frames
+
+    out_tgt_f.close()
+    out_src_f.close()
+    out_tran_f.close()
+
+    print(total_frame / total_cnt)
+    print(total_cnt)
+
 if __name__ == '__main__': 
     # filter fairseq manifest files based on target utterance # of frames 
 
@@ -55,7 +93,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--lan_pair', type=str, default='es-en')
     parser.add_argument('--data_filter_threshold', type=float, default=1.09)
-    parser.add_argument('--frame_threshold', type=int, default=120)
+    parser.add_argument('--frame_threshold', type=int, default=200)
     parser.add_argument('--data_root', type=str, default='/data/sls/temp/clai24/data/speech_matrix/speech_to_unit/s2u_manifests')
     args = parser.parse_args()
    
@@ -66,13 +104,20 @@ if __name__ == '__main__':
     #lan_pair = 'ro-en'
     #data_filter_threshold = 1.07
 
-    read_train_tsv(f'{args.data_root}/{args.lan_pair}/train_mined_t{args.data_filter_threshold}.tsv', 
+    filter_train_mined_tsv(f'{args.data_root}/{args.lan_pair}/train_mined_t{args.data_filter_threshold}.tsv', 
                     f'{args.data_root}/{args.lan_pair}/source_unit/train_mined_t{args.data_filter_threshold}.tsv', 
                     f'{args.data_root}/{args.lan_pair}/train_mined_t{args.data_filter_threshold}_filter{args.frame_threshold}.tsv', 
                     f'{args.data_root}/{args.lan_pair}/source_unit/train_mined_t{args.data_filter_threshold}_filter{args.frame_threshold}.tsv', 
                     args.frame_threshold)
-    read_valid_tsv(f'{args.data_root}/{args.lan_pair}/valid_vp.tsv', 
+    filter_valid_vp_tsv(f'{args.data_root}/{args.lan_pair}/valid_vp.tsv', 
                     f'{args.data_root}/{args.lan_pair}/source_unit/valid_vp.tsv', 
                     f'{args.data_root}/{args.lan_pair}/valid_vp_filter{args.frame_threshold}.tsv', 
                     f'{args.data_root}/{args.lan_pair}/source_unit/valid_vp_filter{args.frame_threshold}.tsv',
                     args.frame_threshold)
+    filter_test_epst_tsv(f'{args.data_root}/{args.lan_pair}/test_epst.tsv', 
+                        f'{args.data_root}/{args.lan_pair}/source_unit/test_epst.tsv', 
+                        f'{args.data_root}/{args.lan_pair}/test_epst.en',
+                        f'{args.data_root}/{args.lan_pair}/test_epst_filter{args.frame_threshold}.tsv', 
+                        f'{args.data_root}/{args.lan_pair}/source_unit/test_epst_filter{args.frame_threshold}.tsv',
+                        f'{args.data_root}/{args.lan_pair}/test_epst_filter{args.frame_threshold}.en',
+                        args.frame_threshold)
